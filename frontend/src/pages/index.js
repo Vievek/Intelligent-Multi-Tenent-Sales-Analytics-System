8f90842ab4bb4549f827be855469090e8d0ec182
Your name <vieveganthaneswaran@gmail.com>
2026-06-30 15:01:58 +0530

    feat: export new pages (SalesManagement, AgentManagement, PendingReviewsPage)

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,4 +2,8 @@ export { default as Login } from './Login';
 export { default as AdminDashboard } from './AdminDashboard';
 export { default as TenantDashboard } from './TenantDashboard';
 export { default as TenantManagement } from './TenantManagement';
+export { default as SalesManagement } from './SalesManagement';
+export { default as AgentManagement } from './AgentManagement';
+export { default as PendingReviewsPage } from './PendingReviewsPage';
 export { default as NotFound } from './NotFound';
+
