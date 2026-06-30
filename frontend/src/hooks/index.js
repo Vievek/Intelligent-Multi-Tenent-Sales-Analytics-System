ee82808f8b0d9670516d993f4853ffc926423f48
Your name <vieveganthaneswaran@gmail.com>
2026-06-30 15:01:54 +0530

    feat: export useAgents and usePendingReviews hooks

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,3 +1,6 @@
 export { useAuth, AuthProvider } from './useAuth';
 export { useSales } from './useSales';
 export { useTenants } from './useTenants';
+export { useAgents } from './useAgents';
+export { usePendingReviews } from './usePendingReviews';
+
